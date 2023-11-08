7fc440e1c7d6e78a99e0ca52369b1edf79e7a328
Sergii <seregaonma@gmail.com>
2023-11-08 19:47:00 +0200

    create House-class with properties and methods

--- js/main.js
+++ js/main.js
@@ -12,3 +12,43 @@ class Apartment {
     this.apartmentMembers.push(person);
   }
 }
+
+class House {
+  constructor(maxAllowedApartm) {
+    this.maxAllowedApartm = maxAllowedApartm;
+  }
+  houseApartments = [];
+
+  addApartment (apartment) {
+    if(this.houseApartments.length < this.maxAllowedApartm) {
+      this.houseApartments.push(apartment);
+    } else {
+      console.log('No more space for the apartment in this house');
+    }
+  }
+}
+
+const personOne = new Human('Sergii', 'male');
+const personTwo = new Human('Viktoria', 'female');
+const personThree = new Human('Viktor', 'male');
+const personFour = new Human('Natali', 'female');
+
+const firstApartment = new Apartment;
+firstApartment.addMember(personOne);
+firstApartment.addMember(personFour);
+
+const secondApartment = new Apartment;
+secondApartment.addMember(personTwo);
+
+const thirdApartment = new Apartment;
+thirdApartment.addMember(personThree);
+
+console.log(firstApartment, secondApartment, thirdApartment);
+
+const firstHouse = new House(2);
+firstHouse.addApartment(firstApartment);
+firstHouse.addApartment(secondApartment);
+console.log(firstHouse);
+
+firstHouse.addApartment(thirdApartment);
+console.log(firstHouse);
